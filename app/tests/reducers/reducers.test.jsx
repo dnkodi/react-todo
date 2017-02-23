2d92129fba57b30667fbdb9fdf25382292b352c5
Dulip <dnkodi@yahoo.com>
2017-02-23 20:12:02 +0530

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -23,4 +23,36 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () =>{
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should change completedAt status', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+
+      var todo = [{
+          id:1,
+          text: 'do something',
+          completed: true,
+          createdAt: 123,
+          completedAt: 125
+      }];
+
+      var res = reducers.todosReducer(df(todo), df(action));
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+  });
 });
